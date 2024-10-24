831c000f2edfde737492917cddac547c28fed45f
ANOUARLAACHIR <become.engineer88@gmail.com>
2024-10-24 21:29:36 +0100

    adding updateStudent() and deleteStudent()  methods

--- src/main/java/com/codewa/spring_rest_crud_ops/controller/StudentController.java
+++ src/main/java/com/codewa/spring_rest_crud_ops/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.codewa.spring_rest_crud_ops.controller;
 
 import com.codewa.spring_rest_crud_ops.entity.Student;
 import com.codewa.spring_rest_crud_ops.repository.StudentRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,7 +31,26 @@ public class StudentController {
 
     //add new student
     @PostMapping("/student/save")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Student saveStudent(@RequestBody Student student) {
         return studentRepository.save(student);
     }
+
+    //delete a student
+    @DeleteMapping("/student/delete/{id}")
+    public void deleteStudent(@PathVariable int id) {
+        Optional <Student> student = studentRepository.findById(id);
+        studentRepository.deleteById(id);
+    }
+
+    //update student
+    @PutMapping("/student/update/{id}")
+    public Student updateStudentById(@PathVariable int id, @RequestBody Student student) {
+        Student updatedStudent = studentRepository.findById(id).get();
+        updatedStudent.setName(student.getName());
+        updatedStudent.setBranch(student.getBranch());
+        updatedStudent.setPercentage(student.getPercentage());
+        return studentRepository.save(updatedStudent);
+    }
+
 }

bd467d76d7f1e3b8471c601ae9bb5548f3ab06e5
ANOUARLAACHIR <become.engineer88@gmail.com>
2024-10-24 13:19:46 +0100

    configure connection to db using application.properties file

---
+++ src/main/java/com/codewa/spring_rest_crud_ops/SpringRestCrudOpsApplication.java
@@ -0,0 +1,13 @@
+package com.codewa.spring_rest_crud_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCrudOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestCrudOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewa/spring_rest_crud_ops/SpringRestCrudOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewa.spring_rest_crud_ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
